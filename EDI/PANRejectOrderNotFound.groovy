d67232b22a1be9de22af6fed458ad9820ebf43cb
pravemarrapu <pravemarrapu@gmail.com>
2017-09-19 13:05:06 +0530

    Praveen : Sync from APM DEV

--- EDI/PANRejectOrderNotFound.groovy
+++ EDI/PANRejectOrderNotFound.groovy
@@ -5,29 +5,30 @@
  *
  *  Modified By: Pradeep Arya
  *  Date 20-Mar-17 WF#801147 - Order validation for pickup
+ *  13-Sept-17 - WF#891531 - ERO validation for dropoff empties
+ *  18-Sept-17 - WF#895061 - ITT - RE RAIL: no error on PREAN unknown booking
  */
 
-package com.navis.apex.groovy.mv2_rwg.preannouncements.validation.bizflow.tasks;
+package com.navis.apex.groovy.mv2_rwg.preannouncements.validation.bizflow.tasks
 
 import com.navis.argo.ContextHelper
-import com.navis.argo.business.reference.Equipment
-import com.navis.argo.business.reference.ScopedBizUnit;
-import com.navis.external.framework.util.ExtensionUtils;
-import com.navis.external.road.AbstractGateTaskInterceptor;
-import com.navis.external.road.EGateTaskInterceptor;
+import com.navis.external.framework.util.ExtensionUtils
+import com.navis.external.road.AbstractGateTaskInterceptor
+import com.navis.external.road.EGateTaskInterceptor
 import com.navis.framework.util.TransactionParms
 import com.navis.framework.util.internationalization.PropertyKeyFactory
-import com.navis.framework.util.internationalization.UserMessage;
-import com.navis.framework.util.message.MessageCollector;
-import com.navis.framework.util.message.MessageCollectorFactory;
+import com.navis.framework.util.message.MessageCollector
+import com.navis.framework.util.message.MessageCollectorFactory
 import com.navis.framework.util.message.MessageLevel
-import com.navis.orders.business.eqorders.EquipmentDeliveryOrder;
-import com.navis.road.RoadPropertyKeys;
+import com.navis.orders.business.eqorders.EquipmentDeliveryOrder
+import com.navis.orders.business.eqorders.EquipmentReceiveOrder
+import com.navis.road.RoadPropertyKeys
 import com.navis.road.business.appointment.model.GateAppointment
-import com.navis.road.business.atoms.GeneralTransactionTypeEnum;
-import com.navis.road.business.util.RoadBizUtil;
-import com.navis.road.business.workflow.IUnitBean;
-import com.navis.road.business.workflow.TransactionAndVisitHolder;
+import com.navis.road.business.atoms.TruckerFriendlyTranSubTypeEnum
+import com.navis.road.business.util.RoadBizUtil
+import com.navis.road.business.workflow.IUnitBean
+import com.navis.road.business.workflow.TransactionAndVisitHolder
+import org.apache.fop.viewer.UserMessage
 
 public class PANRejectOrderNotFound extends AbstractGateTaskInterceptor implements EGateTaskInterceptor {
 
@@ -39,7 +40,7 @@ public class PANRejectOrderNotFound extends AbstractGateTaskInterceptor implemen
 
         executeExternal(inWfCtx.getTran(), inWfCtx.getAppt());
 
-        if (thisTaskMsgCollector.hasError()) {
+        /*if (thisTaskMsgCollector.hasError()) {
             List<UserMessage> severMessages = thisTaskMsgCollector.getMessages(MessageLevel.SEVERE);
             if (severMessages != null && severMessages.size() > 0) {
                 for (UserMessage curretUserMessage : severMessages) {
@@ -53,28 +54,49 @@ public class PANRejectOrderNotFound extends AbstractGateTaskInterceptor implemen
             }
 
 
+        }*/
+        // WF#895061
+        if (thisTaskMsgCollector.hasError()) {
+            if (RoadBizUtil.getFirstErrorMessage(thisTaskMsgCollector).getMessageKey().getKey().equalsIgnoreCase("PANRejectLineNotFound") ||
+                    RoadBizUtil.getFirstErrorMessage(thisTaskMsgCollector).getMessageKey().getKey().equalsIgnoreCase("PANRejectEquipmentNotFound")) {
+                _panGateTaskHelper.recordPreanError(inWfCtx, RoadBizUtil.getFirstErrorMessage(thisTaskMsgCollector).getMessageKey().getKey(), RoadBizUtil.getFirstErrorMessage(thisTaskMsgCollector));
+            }
+            else
+                _panGateTaskHelper.recordPreanError(inWfCtx, getClass().getSimpleName(), RoadBizUtil.getFirstErrorMessage(thisTaskMsgCollector));
         }
         TransactionParms.getBoundParms().setMessageCollector(origMsgCollector);
     }
 
     public void executeExternal(IUnitBean inTran, GateAppointment inPrean) {
-    
+
         String eqoNbr = inTran.getTranEqoNbr() != null ? inTran.getTranEqoNbr() : inPrean.getFieldString(_panFields.PREAN_EQO_NBR);
         String ediEqoNbr = inPrean.getFieldString(_panFields.PREAN_EQO_NBR);
         String apptEqoNbr = inTran.getTranEqoNbr();
-        log("ediEqoNbr:$ediEqoNbr + apptEqoNbr:$apptEqoNbr");
+        //log("ediEqoNbr:$ediEqoNbr + apptEqoNbr:$apptEqoNbr");
         String lineId = inTran.getTranLineId();
-        log("ediEqoNbr:$ediEqoNbr + apptEqoNbr:$apptEqoNbr :: line Id:: $lineId Equipment Selected :: "+inTran.getTranEq() + " Tran EQO :: "+inTran.getTranEqo());
+        log("ediEqoNbr:$ediEqoNbr + apptEqoNbr:$apptEqoNbr :: line Id:: $lineId Equipment Selected :: "+inTran.getTranEq());
         //if DM already reserved against an EDO N4 always populate the transaction with that
         //checking if tran/appt EDO are different and error out if the edi EDO doesn't exist
-        if (apptEqoNbr && ediEqoNbr && ediEqoNbr != apptEqoNbr) {
-            EquipmentDeliveryOrder eqo = EquipmentDeliveryOrder.findEquipmentDeliveryOrder(ediEqoNbr, inTran.getTranLine());
-            if (eqo == null) {
-                inTran.setTranEqoNbr(ediEqoNbr);
-                RoadBizUtil.getMessageCollector().appendMessage(MessageLevel.SEVERE,
-                        RoadPropertyKeys.GATE__ORDER_NOT_FOUND, null, [eqoNbr]);
+        if(apptEqoNbr && ediEqoNbr && ediEqoNbr != apptEqoNbr) {
+            //WF#891531 - For RM error out if ERO does not exist
+            if(inTran.isReceival() && inPrean.getGapptTranType() == TruckerFriendlyTranSubTypeEnum.DOM) {
+                EquipmentReceiveOrder ero = EquipmentReceiveOrder.findEroByUniquenessCriteria(eqoNbr, inTran.getTranLine());
+                if (ero == null) {
+                    inTran.setTranEqoNbr(eqoNbr);
+                    RoadBizUtil.getMessageCollector().appendMessage(MessageLevel.SEVERE,
+                            RoadPropertyKeys.GATE__ORDER_NOT_FOUND, null, [eqoNbr]);
+                }
             }
-        } else if (eqoNbr != null && inTran.getTranEqo() == null) {
+            else {
+                EquipmentDeliveryOrder eqo = EquipmentDeliveryOrder.findEquipmentDeliveryOrder(ediEqoNbr, inTran.getTranLine());
+                if (eqo == null) {
+                    inTran.setTranEqoNbr(ediEqoNbr);
+                    RoadBizUtil.getMessageCollector().appendMessage(MessageLevel.SEVERE,
+                            RoadPropertyKeys.GATE__ORDER_NOT_FOUND, null, [eqoNbr]);
+                }
+            }
+        }
+        else if (eqoNbr != null && inTran.getTranEqo() == null) {
 
             if (lineId != null) {
                 RoadBizUtil.getMessageCollector().appendMessage(MessageLevel.SEVERE,
@@ -93,11 +115,9 @@ public class PANRejectOrderNotFound extends AbstractGateTaskInterceptor implemen
         if (inTran.getTranEq() != null && inTran.getTranEq().getEqEquipType() != null && inTran.getTranEq().getEqEquipType().isUnknownType()) {
             RoadBizUtil.getMessageCollector().appendMessage(MessageLevel.SEVERE,
                     PropertyKeyFactory.valueOf("PANRejectEquipmentNotFound"), null, null);
-
-5        }
+        }
     }
 
-    private static
-    def _panGateTaskHelper = ExtensionUtils.getLibrary(ContextHelper.getThreadUserContext(), "PANGateTaskHelper");
+    private static def _panGateTaskHelper = ExtensionUtils.getLibrary(ContextHelper.getThreadUserContext(), "PANGateTaskHelper");
     private static def _panFields = ExtensionUtils.getLibrary(ContextHelper.getThreadUserContext(), "PANFields");
 }
